28d7acc764e8e5890db689da10e85928dfb5d024
andreunsgaard <aunsgaard@live.com>
2016-10-04 10:47:49 +0200

    fileredModular

--- myfirstAsyncio.js
+++ myfirstAsyncio.js
@@ -1,15 +1,15 @@
-var fs = require('fs')
+var fs = require('fs');
 fs.readFile(process.argv[2],
 	function callback(error, data) {
 		if (error) {
-			console.log(error)
+			console.log(error);
 		} else {
 
-			var str = data.toString()
+			var str = data.toString();
 
-			var arrayLineas = str.split('\n')
+			var arrayLineas = str.split('\n');
 
-			console.log(arrayLineas.length - 1)
+			console.log(arrayLineas.length - 1);
 		}
 	}
-)
+);
